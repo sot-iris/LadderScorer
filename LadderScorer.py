217d5e087532e9f406150c7509a81874b5a051f9
Sotiris <skakanos@hotmail.co.uk>
2020-02-21 14:34:26 +0000

    Update LadderScorer.py

--- LadderScorer.py
+++ LadderScorer.py
@@ -219,15 +219,19 @@ class LadderAnalysis:
                     fit_x.append(i)
                     fit_y.append(new.iloc[i].astype('float')[f'{feature}_y'])
 
+            #xnew = np.arange(min(fit_x), max(fit_x), 1)
+            #print(f'xnew  = {min(xnew)}')
+            #print(f'fit_x  = {min(fit_x)}')
+            #f = interp1d(fit_x, fit_y)
+            #ynew = f(xnew)
+
             if runNum==run:
-                pass
-                #plt.figure(figsize = (40, 5))
-                #plt.gca().invert_yaxis()
+                plt.figure(figsize = (40, 5))
+                plt.gca().invert_yaxis()
                 #plt.scatter(fit_x, fit_y, "o", xnew, ynew, "-")
             elif runNum == "All":
-                pass
-                #plt.figure(figsize = (40, 5))
-                #plt.gca().invert_yaxis()
+                plt.figure(figsize = (40, 5))
+                plt.gca().invert_yaxis()
                 #plt.scatter(fit_x, fit_y, "o", xnew, ynew, "-")
 
             run_v += 2
